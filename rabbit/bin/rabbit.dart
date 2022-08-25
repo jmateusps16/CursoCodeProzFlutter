ae5aedc96fd4f954db41cbbc8a15abdddd12003b
Mateus Palmeira <jmateusps16@gmail.com>
2022-08-24 22:38:32 -0300

    Semana 4

---
+++ rabbit/bin/rabbit.dart
@@ -0,0 +1,5 @@
+import 'package:rabbit/rabbit.dart' as rabbit;
+
+void main(List<String> arguments) {
+  print('Hello world: ${rabbit.calculate()}!');
+}
---
+++ rabbit/test/rabbit_test.dart
@@ -0,0 +1,8 @@
+import 'package:rabbit/rabbit.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
